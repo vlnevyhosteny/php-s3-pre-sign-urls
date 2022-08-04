249c9807949045f794a6d3e2b091764941055715
Vladimír Nevyhoštěný <vnevyhosteny@gmail.com>
2022-08-04 17:23:52 +0200

    feat: get presign url from s3

--- index.php
+++ index.php
@@ -1,1 +1,33 @@
-<p>Hello world!</p>
+<?php
+
+use Aws\S3\S3Client;
+use League\Flysystem\AwsS3v3\AwsS3Adapter;
+use League\Flysystem\Filesystem;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$credentials = new Aws\Credentials\Credentials('access_key', 'secret_key');
+
+$client = new Aws\S3\S3Client([
+  'endpoint' => 'http://localhost:9000',
+  'credentials' => $credentials,
+  'region' => 'us-west-2',
+  'version' => 'latest',
+  'use_path_style_endpoint' => true,
+]);
+
+// The internal adapter
+$adapter = new League\Flysystem\AwsS3v3\AwsS3Adapter(
+    $client,
+    'my-bucket'
+);
+
+// The FilesystemOperator
+$filesystem = new League\Flysystem\Filesystem($adapter);
+$filesystem->addPlugin(new Etime\Flysystem\Plugin\AWS_S3\PresignedUrl());
+
+$myFile = 'kitty.jpeg';
+
+$url = $filesystem->getPresignedUrl($myFile);
+
+echo $url;
